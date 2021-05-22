d102775c5f4f6f86bb6c7b6b3486d18e48fd3e77
Sougata Ghosh <sougataghosh314@gmail.com>
2021-05-22 17:30:54 +0530

    passing functions to child components

--- src/Home.js
+++ src/Home.js
@@ -2,21 +2,21 @@ import { useState } from "react";
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBolgs] = useState([
+    const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'Bruce', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Clark', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Dianna', id: 3 },
         { title: 'Spring top tips', body: 'lorem ipsum dolor...', author: 'Clark', id: 4 }
     ]);
 
-    // const handleDelete = (id) => {
-        
-    // }
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((blog) => blog.id != id);
+        setBlogs(newBlogs);
+    }
 
     return (
         <div className="home">
-            <BlogList blogs={ blogs } title="All Blogs:"/>
-            <BlogList blogs={ blogs.filter((blog) => blog.author === "Clark") } title="Clark's blog(s):"/>
+            <BlogList blogs={ blogs } title="All Blogs:" handleDelete={handleDelete}/>
         </div>
     );
 }
