06058f42286dbf560c1158e5890666a9c6458edd
Sougata Ghosh <sougataghosh314@gmail.com>
2021-05-22 17:26:30 +0530

    filter method for list

--- src/BlogList.js
+++ src/BlogList.js
@@ -2,6 +2,8 @@ const BlogList = ({blogs, title}) => {
     // const blogs = props.blogs;
     // const title = props.title;
 
+    
+
     return (
         <div className="blog-list">
             <h2>{title}</h2>
@@ -9,7 +11,7 @@ const BlogList = ({blogs, title}) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Written by: { blog.author }</p>
-                    
+                    {/* <button onClick={() => handleDelete((blog.id)}>delete</button> */}
                 </div>
             )) }
         </div>
--- src/Home.js
+++ src/Home.js
@@ -5,14 +5,20 @@ const Home = () => {
     const [blogs, setBolgs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'Bruce', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Clark', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Dianna', id: 3 }
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Dianna', id: 3 },
+        { title: 'Spring top tips', body: 'lorem ipsum dolor...', author: 'Clark', id: 4 }
     ]);
+
+    // const handleDelete = (id) => {
+        
+    // }
 
     return (
         <div className="home">
             <BlogList blogs={ blogs } title="All Blogs:"/>
+            <BlogList blogs={ blogs.filter((blog) => blog.author === "Clark") } title="Clark's blog(s):"/>
         </div>
     );
 }
- 
+
 export default Home;
