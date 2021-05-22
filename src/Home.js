e39b7366c84b9b80083a28fd5ee3a91481cf21e2
Sougata Ghosh <sougataghosh314@gmail.com>
2021-05-22 17:02:11 +0530

    list as a reactive component

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,21 @@
 import { useState } from "react";
 
 const Home = () => {
-    //let name = "Clark";
-    const [name, setName] = useState("Clark");
-    const [age, setAge] = useState(36);
-
-    const handleClick = (e) => {
-        setName("Bruce");
-        setAge(48);
-    }
+    const [blogs, setBolgs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'Bruce', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Clark', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Dianna', id: 3 }
+    ]);
 
     return (
         <div className="home">
-            <h2>Home page</h2>
-            <p>{ name } is { age } years old.</p>
-            <button onClick={handleClick}>Click me</button>
+            { blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by: { blog.author }</p>
+                    
+                </div>
+            )) }
         </div>
     );
 }
