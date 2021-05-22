3a44bf046b4c6befafa8022a88b36d773ae2941b
Sougata Ghosh <sougataghosh314@gmail.com>
2021-05-22 18:48:37 +0530

    custom hooks to fetch data, pending, error from whatever url you like: more reusable code this way

---
+++ src/useFetch.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch(url)
+            .then(res => {
+                console.log(res);
+                if(!res.ok) {
+                    throw Error("Could not fetch the data for that resource.");
+                }
+                return res.json();
+            })
+            .then((data) => {
+                console.log(data);
+                setData(data);
+                setIsLoading(false);
+                setError(null);
+            })
+            .catch((err) => {
+                setError(err.message);
+                setIsLoading(false);
+            });
+        }, 600);
+    }, [url]);
+
+    return { data, isLoading, error };
+}
+
+export default useFetch;
